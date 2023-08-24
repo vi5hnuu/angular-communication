8962af1147a6c7ff6dd1f8239afda2703f12f8fb
vishnukumar <vishnu.kumar@digio.in>
2023-08-24 21:24:45 +0530

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {ProductDetailComponent} from "./products/product-detail/product-detail.component";
+import {ProductListComponent} from "./products/product-list/product-list.component";
+import {ProductFormComponent} from "./products/product-form/product-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component: HomeComponent},
+  {path:'login',component: LoginComponent},
+  {path:'home/welcome',component: WelcomeComponent,outlet:'home-outlet'},
+  {path:'home/products',component: ProductListComponent,outlet:'home-outlet'},
+  {path:'products/:id',component: ProductDetailComponent},
+  {path:'edit-product',component: ProductFormComponent},
+  {path:'add-product',component: ProductFormComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'**',redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {RatingStarComponent} from "./shared/rating-star/rating-star.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {LoginComponent} from "./login/login.component";
+import {HomeComponent} from "./home/home.component";
+import {ProductAddComponent} from "./products/product-add/product-add.component";
+import {ProductListComponent} from "./products/product-list/product-list.component";
+import { ProductFormComponent } from './products/product-form/product-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDetailComponent,
+    ProductListComponent,
+    ProductAddComponent,
+    HomeComponent,
+    LoginComponent,
+    WelcomeComponent,
+    RatingStarComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
